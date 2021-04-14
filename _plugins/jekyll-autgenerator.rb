0c74f184cddee94ac604decf3532249fc1ee4ba7
mgilbert <matt.b.gilbert@gmail.com>
2021-04-14 10:50:15 -0400

    first step multi authors

--- _plugins/jekyll-autgenerator.rb
+++ _plugins/jekyll-autgenerator.rb
@@ -41,7 +41,7 @@ module Jekyll
     private
 
     def posts_by_author(site, author)
-      site.posts.docs.select { |post| post.data['author'] == author }
+      site.posts.docs.select { |post| post.data['author'].include? author }
     end
   end
 
