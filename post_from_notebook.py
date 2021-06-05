0d6b03542503a884ef2f211eb52717772b1ce6e3
Nikhil <nikhil.woodruff@outlook.com>
2021-06-05 17:20:17 +0100

    Almost fully working

--- post_from_notebook.py
+++ post_from_notebook.py
@@ -4,10 +4,26 @@ from argparse_prompt import PromptParser
 from typing import IO
 import yaml
 
+from pathlib import Path
+import json
+from argparse import ArgumentParser
+from typing import IO
+
 HEADER = """
 <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 <script src="https://requirejs.org/docs/release/2.3.5/minified/require.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+"""
+
+SCRIPT_TEMPLATE = """
+<div>
+  <script>
+    $(document).ready(function(){
+      $("#graph_{id}").load("{{site.baseurl}}{asset_filename}/graph_{id}.html");
+    });
+  </script>
+</div>
+<div id = "graph_{id}"></div>
 """
 
 CODE_BLOCK = """
@@ -29,7 +45,8 @@ function show_code_{id}() {
     x.style.display = "none";
   }
 }
-</script> \n"""
+</script>
+"""
 
 class NotebookCell:
     def __init__(self, data):
@@ -37,7 +54,7 @@ class NotebookCell:
         self.id = self.data["id"].replace("-", "_")
         self.cell_type = self.data["cell_type"]
     
-    def render(self, out_file: IO):
+    def render(self, out_file: IO, asset_folder: Path):
         if self.cell_type == "markdown":
             # write markdown to file
             for line in self.data["source"]:
@@ -48,30 +65,32 @@ class NotebookCell:
             for line in formatted_code_block:
                 out_file.write(line)
             # write outputs
-            for output in self.data["outputs"]:
-                # write text/html
-                out_file.write("".join(output["data"]["text/html"]).lstrip())
-
+            output = self.data["outputs"][0]["data"]["text/html"]
+            html = "".join(output)
+            asset_filename = asset_folder / f"graph_{str(self.id)}.html"
+            asset_filename.parent.mkdir(parents=True, exist_ok=True)
+            out_file.write(SCRIPT_TEMPLATE.replace("{asset_filename}", str(asset_filename)).replace("{id}", str(self.id)))
+            with open(asset_filename, "w+") as f:
+                f.write(html)
 
 class NotebookPost:
     def __init__(self, path: Path, metadata: dict):
         self.metadata = metadata
         with open(path) as f:
             self.data = json.load(f)
-        self.cells = map(NotebookCell, self.data["cells"])
+        self.cells = list(map(NotebookCell, self.data["cells"]))
     
-    def render(self, out_file_path: Path):
-        if isinstance(out_file_path, str):
-            out_file_path = Path(out_file_path)
-        if not out_file_path.parent.exists():
-            out_file_path.parent.mkdir(parents=True, exist_ok=True) 
+    def render(self, out_file_path: Path, asset_folder: Path):
+        out_file_path = Path(out_file_path)
+        asset_folder = Path(asset_folder)
+        out_file_path.parent.mkdir(parents=True, exist_ok=True)
         with open(out_file_path, "w+") as f:
             f.write("---\n")
             yaml.safe_dump(self.metadata, f)
             f.write("---\n")
             f.write(HEADER)
             for cell in self.cells:
-                cell.render(f)
+                cell.render(f, asset_folder)
 
 if __name__ == "__main__":
     parser = PromptParser(description="A utility for converting Jupyter Notebooks into UBI Center posts.")
@@ -86,7 +105,8 @@ if __name__ == "__main__":
     parser.add_argument("--class_", default="post-template")
     parser.add_argument("--subclass", default="'post'")
     parser.add_argument("--author", default="max", help="The author name")
-    parser.add_argument("--output-file", default="post.md")
+    parser.add_argument("--output-file", default="_posts/2021-05-04-post.md")
+    parser.add_argument("--asset-folder", default="assets/markdown_assets/libdem")
     args = parser.parse_args()
 
     metadata = dict(
@@ -103,4 +123,4 @@ if __name__ == "__main__":
     metadata["class"] = args.class_
 
     notebook = NotebookPost(args.notebook, metadata)
-    notebook.render(args.output_file)
+    notebook.render(args.output_file, args.asset_folder)
