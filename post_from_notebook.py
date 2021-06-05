b01ea19c4b8b726afe52729f4907d8f20efb746d
Nikhil <nikhil.woodruff@outlook.com>
2021-06-05 17:41:23 +0100

    Final working script tool

--- post_from_notebook.py
+++ post_from_notebook.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import json
 from argparse_prompt import PromptParser
 from typing import IO
-import yaml
 
 from pathlib import Path
 import json
@@ -55,6 +54,7 @@ class NotebookCell:
         self.cell_type = self.data["cell_type"]
     
     def render(self, out_file: IO, asset_folder: Path):
+        out_file.write("\n")
         if self.cell_type == "markdown":
             # write markdown to file
             for line in self.data["source"]:
@@ -86,7 +86,8 @@ class NotebookPost:
         out_file_path.parent.mkdir(parents=True, exist_ok=True)
         with open(out_file_path, "w+") as f:
             f.write("---\n")
-            yaml.safe_dump(self.metadata, f)
+            for key, value in self.metadata.items():
+              f.write(f"{key}: {value}\n")
             f.write("---\n")
             f.write(HEADER)
             for cell in self.cells:
@@ -97,14 +98,16 @@ if __name__ == "__main__":
     parser.add_argument("notebook", help="The notebook file to convert.")
     parser.add_argument("--layout", default="post", help="The layout style")
     parser.add_argument("--current", default="post")
-    parser.add_argument("--cover", default="assets/images/bear.jpg", help="The cover image path")
+    parser.add_argument("--cover", default="", help="The cover image path")
     parser.add_argument("--navigation", default=True)
     parser.add_argument("--title", default="Title goes here", help="The title")
-    parser.add_argument("--date", default="2012-09-01 10:00:00", help="The publishing date")
-    parser.add_argument("--tags", default="", help="The tags")
+    parser.add_argument("--date", default="2012-09-01", help="The publishing date")
+    parser.add_argument("--tags", default="[]", help="The tags")
     parser.add_argument("--class_", default="post-template")
     parser.add_argument("--subclass", default="'post'")
     parser.add_argument("--author", default="max", help="The author name")
+    parser.add_argument("--excerpt", default="Excerpt goes here", help="The excerpt to show on the website")
+
     parser.add_argument("--output-file", default="_posts/2021-05-04-post.md")
     parser.add_argument("--asset-folder", default="assets/markdown_assets/libdem")
     args = parser.parse_args()
