0f9da16d4cd31498e2194414bd96cb2088243b25
Nikhil <nikhil.woodruff@outlook.com>
2021-06-15 18:30:35 +0100

    Fix scripting bugs

--- post_from_notebook.py
+++ post_from_notebook.py
@@ -1,12 +1,8 @@
 from pathlib import Path
 import json
-from argparse_prompt import PromptParser
-from typing import IO
-
-from pathlib import Path
-import json
 from argparse import ArgumentParser
 from typing import IO
+import yaml
 
 HEADER = """
 <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
@@ -48,9 +44,9 @@ function show_code_{id}() {
 """
 
 class NotebookCell:
-    def __init__(self, data):
+    def __init__(self, id, data):
         self.data = data
-        self.id = self.data["id"].replace("-", "_")
+        self.id = str(id)
         self.cell_type = self.data["cell_type"]
     
     def render(self, out_file: IO, asset_folder: Path):
@@ -80,7 +76,7 @@ class NotebookPost:
         self.metadata = metadata
         with open(path) as f:
             self.data = json.load(f)
-        self.cells = list(map(NotebookCell, self.data["cells"]))
+        self.cells = list(map(lambda x: NotebookCell(x[0], x[1]), enumerate(self.data["cells"])))
     
     def render(self, out_file_path: Path, asset_folder: Path):
         out_file_path = Path(out_file_path)
@@ -88,44 +84,23 @@ class NotebookPost:
         out_file_path.parent.mkdir(parents=True, exist_ok=True)
         with open(out_file_path, "w+") as f:
             f.write("---\n")
-            for key, value in self.metadata.items():
-              f.write(f"{key}: {value}\n")
-            f.write("---\n")
+            for line in self.metadata:
+              f.write(line)
+            f.write("\n---\n")
             f.write(HEADER)
             for cell in self.cells:
                 cell.render(f, asset_folder)
 
 if __name__ == "__main__":
-    parser = PromptParser(description="A utility for converting Jupyter Notebooks into UBI Center posts.")
+    parser = ArgumentParser(description="A utility for converting Jupyter Notebooks into UBI Center posts.")
     parser.add_argument("notebook", help="The notebook file to convert.")
-    parser.add_argument("--layout", default="post", help="The layout style")
-    parser.add_argument("--current", default="post")
-    parser.add_argument("--cover", default="", help="The cover image path")
-    parser.add_argument("--navigation", default=True)
-    parser.add_argument("--title", default="Title goes here", help="The title")
-    parser.add_argument("--date", default="2012-09-01", help="The publishing date")
-    parser.add_argument("--tags", default="[]", help="The tags")
-    parser.add_argument("--class_", default="post-template")
-    parser.add_argument("--subclass", default="'post'")
-    parser.add_argument("--author", default="max", help="The author name")
-    parser.add_argument("--excerpt", default="Excerpt goes here", help="The excerpt to show on the website")
-    parser.add_argument("--output-file", default="_posts/2021-05-04-post.md")
-    parser.add_argument("--asset-folder", default="assets/markdown_assets/post")
+    parser.add_argument("metadata", help="A YAML file containing the post metadata")
+    parser.add_argument("--output-md", help="The markdown file to write", default="post.md")
+    parser.add_argument("--output-folder", help="The folder to write html resources to", default="resources/")
     args = parser.parse_args()
 
-    metadata = dict(
-        layout=args.layout,
-        current=args.current,
-        cover=args.cover,
-        navigation=args.navigation,
-        title=args.title,
-        date=args.date,
-        tags=args.tags,
-        subclass=args.subclass,
-        author=args.author,
-        excerpt=args.excerpt
-    )
-    metadata["class"] = args.class_
+    with open(args.metadata) as f:
+      metadata = f.readlines()
 
     notebook = NotebookPost(args.notebook, metadata)
-    notebook.render(args.output_file, args.asset_folder)
+    notebook.render(args.output_md, args.output_folder)
